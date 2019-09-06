86725e9e1d0457c9fc99bd722edb83d54884c358
Mukhammadsher <mr.muhammadsher@mail.ru>
2019-09-06 16:25:49 +0500

    Notification update status

--- modules/notification_module.js
+++ modules/notification_module.js
@@ -1,5 +1,15 @@
 "use strict";
 
+/*
+ * default listener `notification` called once returned in each this.Parent.CONFIG.NOTIFICATION_INTERVAL
+ * params {jwt token}
+ */
+
+/*
+ * for updating notification status emit and listen `updateNotificationStatus`
+ * params {jwt token, action, id} - id -> notification id
+ */
+
 var jwt = require("jsonwebtoken");
 
 class fsNotification {
@@ -10,32 +20,55 @@ class fsNotification {
     this.maxId = 0;
   }
 
-  run(post) {
-    this.Parent.DB.connect();
-
-    var interval = setInterval(() => {
-      jwt.verify(post.token, this.Parent.CONFIG.SECRET, (err, decoded) => {
+  updateNotificationStatus(post, decoded) {
+    this.Parent.DB.query(
+      `UPDATE fizmasoft_notification SET acknowledged = TRUE WHERE user_id = ${decoded.user[0].id} AND id = ${post.id}`,
+      err => {
         if (err) {
-          this.socket.emit("err", { status: 401, message: "Unauthorized" });
-          clearInterval(interval);
-          this.Parent.DB.disconnect();
+          this.socket.emit("err", err);
         } else {
-          this.Parent.DB.query(
-            `SELECT id, created_time, args FROM fizmasoft_notification WHERE user_id='${decoded.user[0].id}' AND NOT acknowledged ORDER BY id DESC;`,
-            (err, res) => {
-              if (err) {
-                this.socket.emit("err", err);
-              } else {
-                if (res.rowCount > 0 && this.maxId < res.rows[0].id) {
-                  this.maxId = res.rows[0].id;
-                  this.socket.emit(this.Path, res.rows);
-                }
-              }
-            }
-          );
+          this.socket.emit("updateNotificationStatus", {
+            ok: true,
+            id: post.id
+          });
         }
-      });
-    }, parseInt(this.Parent.CONFIG.NOTIFICATION_INTERVAL));
+      }
+    );
+  }
+
+  run(post, decodedJWT) {
+    this.Parent.DB.connect();
+    switch (post.action) {
+      case "updateNotificationStatus":
+        this.updateNotificationStatus(post, decodedJWT);
+        break;
+      default:
+        // notification listener
+        var interval = setInterval(() => {
+          jwt.verify(post.token, this.Parent.CONFIG.SECRET, (err, decoded) => {
+            if (err) {
+              this.socket.emit("err", { status: 401, message: "Unauthorized" });
+              clearInterval(interval);
+              this.Parent.DB.disconnect();
+            } else {
+              this.Parent.DB.query(
+                `SELECT id, created_time, args FROM fizmasoft_notification WHERE user_id='${decoded.user[0].id}' AND NOT acknowledged ORDER BY id DESC;`,
+                (err, res) => {
+                  if (err) {
+                    this.socket.emit("err", err);
+                  } else {
+                    if (res.rowCount > 0 && this.maxId < res.rows[0].id) {
+                      this.maxId = res.rows[0].id;
+                      this.socket.emit(this.Path, res.rows);
+                    }
+                  }
+                }
+              );
+            }
+          });
+        }, parseInt(this.Parent.CONFIG.NOTIFICATION_INTERVAL));
+        break;
+    }
   }
 }
 
